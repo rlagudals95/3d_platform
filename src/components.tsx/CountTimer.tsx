210491f46dcee707e76a42d898c341866b68f5ab
gudals95 <102935090+gudals95@users.noreply.github.com>
2023-01-11 21:13:51 +0900

    first commit

---
+++ src/components.tsx/CountTimer.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState, useCallback } from "react";
+import { SEX } from "@/constants";
+import { useRouter } from "next/router";
+
+const CountTimer = () => {
+  const router = useRouter();
+  const [lifeSeconds, setLifeSeconds] = useState(0);
+
+  const lifeExpectancy = useCallback(() => {
+    const birthday = localStorage.getItem("birthday") as unknown as string;
+    if (!birthday) {
+      router.replace("/");
+      return
+    }
+    const lifeExpectancyBySex =
+      localStorage.getItem("sex") === SEX.FEMALE ? 80 : 83;
+
+    // 기대 수명
+    const lifeExpectancyDate =
+      `${String(parseInt(birthday?.substring(0, 4)) + lifeExpectancyBySex)}` +
+      String(birthday?.substr(4, 8));
+
+    const lifeExpectancySeconds = new Date(lifeExpectancyDate).getTime() / 1000;
+
+    // 현재
+    const now = Math.floor(new Date().getTime() / 1000);
+
+    // 남은 수명
+    const _livedSeconds = lifeExpectancySeconds - now;
+
+    setLifeSeconds(_livedSeconds);
+  }, []);
+
+  useEffect(() => {
+    lifeExpectancy();
+    const myInterval = setInterval(() => {
+      setLifeSeconds((lifeSeconds) => lifeSeconds - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(myInterval);
+    };
+  }, []);
+
+  return <div className="font-bold">{lifeSeconds}</div>;
+};
+
+export default CountTimer;
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+import CountTimer from "@/components.tsx/CountTimer";
+
+const MainPage = () => {
+  return (
+    <div className="grid h-screen place-items-center">
+      <CountTimer />
+    </div>
+  );
+};
+
+export default MainPage;
